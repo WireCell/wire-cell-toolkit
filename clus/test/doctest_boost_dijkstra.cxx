819f284010031f45b1bfc228a24b2fbe970992dc
HaiwangYu <yuhw.pku@gmail.com>
2024-07-24 15:56:18 -0400

    smaller test

--- clus/test/doctest_boost_dijkstra.cxx
+++ clus/test/doctest_boost_dijkstra.cxx
@@ -9,39 +9,26 @@
 #include <iostream>
 
 // Define graph and property types
+struct EdgeProp {
+    int dist;
+};
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property, EdgeProp> Graph;
+typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+typedef boost::graph_traits<Graph>::edge_descriptor Edge;
 
-namespace TESTFacade {
-    using namespace boost;
-    struct VertexProp {
-        int index;
-    };
-    struct EdgeProp {
-        float dist;
-    };
-    typedef adjacency_list<vecS, vecS, undirectedS, VertexProp, EdgeProp> MCUGraph;
-    typedef graph_traits<MCUGraph>::vertex_descriptor vertex_descriptor;
-    typedef graph_traits<MCUGraph>::edge_descriptor edge_descriptor;
-
-    namespace Cluster {
-        std::vector<vertex_descriptor> parents;
-        std::vector<int> distances;
-    }  // namespace Cluster
-}  // namespace Facade
-
-using namespace TESTFacade;
-using namespace TESTFacade::Cluster;
-TEST_CASE("standalone dijkstra")
-{
-    MCUGraph graph(5);
+TEST_CASE("standalone dijkstra") {
+    Graph graph;
     // Add vertices and edges here
     // ...
-    parents.resize(boost::num_vertices(graph));
-    distances.resize(boost::num_vertices(graph));
-    vertex_descriptor v0 = vertex((size_t) 1, graph);  // Starting vertex
-    const auto& param =
-    boost::weight_map(boost::get(&EdgeProp::dist, graph))
-    .predecessor_map(&parents[0])
-    .distance_map(&distances[0]);
-    std::cout << "param's C++ type: " << typeid(param).name() << std::endl;
-    boost::dijkstra_shortest_paths(graph, v0, param);
+    std::vector<Vertex> parents(boost::num_vertices(graph));
+    std::vector<int> distances(boost::num_vertices(graph));
+    Vertex v0 = 0;  // Starting vertex
+    boost::dijkstra_shortest_paths(
+        graph, v0,
+        boost::weight_map(boost::get(&EdgeProp::dist, graph))
+            .predecessor_map(&parents[0])
+            .distance_map(&distances[0])
+    );
+    // Process results here
+    // ...
 }
