5196fe088d47c80b5ace0106e013e7a1cf2e5df0
HaiwangYu <yuhw.pku@gmail.com>
2025-03-12 23:10:44 -0500

    extract header

---
+++ pytorch/inc/WireCellPytorch/Torch.h
@@ -0,0 +1,13 @@
+
+
+#ifndef WIRECELL_TORCH
+#define WIRECELL_TORCH
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wvariadic-macros"
+#include <torch/script.h>
+// #include <torch/torch.h>
+#pragma GCC diagnostic pop
+
+
+#endif
