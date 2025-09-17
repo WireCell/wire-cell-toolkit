f0a86ddac9caa78729426cb9d3fe1915efe5a6c6
HaiwangYu <yuhw.pku@gmail.com>
2025-09-16 21:42:12 -0500

    compiles and working

--- waft/wcb.py
+++ waft/wcb.py
@@ -44,10 +44,9 @@ package_descriptions = [
     ('ZYRE',     dict(incs=["zyre.h"], libs=['zyre'], pcname='libzyre', mandatory=False)),
     ('ZIO',      dict(incs=["zio/node.hpp"], libs=['zio'], pcname='libzio', mandatory=False,
                       extuses=("ZYRE","CZMQ","ZMQ"))),
-
-    ('GRPC', dict(incs=['grpcpp/grpcpp.h'], libs=['grpc++', 'grpc', 'gpr'], pcname='grpc++')),
-    ('PROTOBUF', dict(incs=['google/protobuf/message.h'], libs=['protobuf'], pcname='protobuf')),
-    ('TRITON', dict(incs=['grpc_client.h'],   
+    ('GRPC',     dict(incs=['grpcpp/grpcpp.h'], libs=['grpc++', 'grpc', 'gpr'], pcname='grpc++', mandatory=False)),
+    ('PROTOBUF', dict(incs=['google/protobuf/message.h'], libs=['protobuf'], pcname='protobuf', mandatory=False)),
+    ('TRITON',   dict(incs=['grpc_client.h'],   
     libs=[
         'grpcclient',
         'tritoncommonerror',
@@ -166,7 +165,8 @@ def configure(cfg):
             ("cuda","HAVE_CUDA"),
             ("hio", "INCLUDES_HDF5"),
             ("pytorch", "LIB_LIBTORCH"),
-            ("zio", "LIB_ZIO LIB_ZYRE LIB_CZMQ LIB_ZMQ")
+            ("zio", "LIB_ZIO LIB_ZYRE LIB_CZMQ LIB_ZMQ"),
+            ("triton", "LIB_GRPC LIB_PROTOBUF LIB_TRITON"), # LIB_PROTOBUF LIB_TRITON
     ]:
         exts = to_list(ext)
         for have in exts:
