950099de10238d494434a911e33a60f725e639cc
physnerds <amitbashyal@gmail.com>
2025-11-10 17:27:21 -0500

    some simple plotting utilities

---
+++ spng/test/pytools/plot_torch_tensors.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as numpy
+import matplotlib.pyplot as plt
+
+# Usage : alias tpython=/nfs/data/1/abashyal/spng/spng_dev_08252025/.direnv/python-3.11.9/bin/python
+# Usage: tpython plot_torch_tensors.py file1.pt file2.pt ...
+
+import sys
+for tensor_file in sys.argv[1:]:
+    tensor = torch.jit.load(tensor_file)
+    print(f"Loaded tensor from {tensor_file}")
+    ## get the param
+    param = next(tensor.parameters())
+    print(f"Parameter from {tensor_file} has shape {param.shape} and dtype {param.dtype}")
+    # if arr in gpu, move to cpu
+    if param.is_cuda:
+        param = param.cpu()
+    arr = param.detach().numpy()
+    print(f"Numpy array shape: {arr.shape}, dtype: {arr.dtype}")
+    # if the shape is 4D, squeeze to 2D for plotting
+    if arr.ndim == 4:
+        #if the first two dims are 1 squeeze them
+        if arr.shape[0] == 1 and arr.shape[1] == 1:
+            arr = arr.squeeze()
+        else:
+            continue
+    if arr.ndim==3:
+        if arr.shape[0]==1:
+            arr = arr.squeeze()
+        else:
+            continue
+        print(f"Squeezed array shape to {arr.shape} for plotting")
+    # Make sure that the dim 0 is always channel dim (i.e., number of wires)
+    if arr.shape[0] > arr.shape[1]:
+        arr = arr.T
+        print(f"Transposed array shape to {arr.shape} for plotting")
+    # Plot the array as an image
+    plt.imshow(arr, aspect='auto', cmap='viridis')
+    plt.colorbar()
+    plt.title(f'Tensor from {tensor_file}')
+    plt.xlabel('Ticks')
+    plt.ylabel('Channels')
+    # save as pnt file
+    plt.savefig(f'{tensor_file}_plot.png')
+    plt.clf()
+    print(f"Plot saved to {tensor_file}_plot.png")
