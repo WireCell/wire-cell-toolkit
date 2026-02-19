89de2c0c5bbfa1fffb356c0d3cb1561f9ab9ebdc
Brett Viren <brett.viren@gmail.com>
2026-02-19 12:28:20 -0500

    Fix a regression due to the added options in rebaseline() changing the default behavior in the test.

--- spng/inc/WireCellSpng/Rebaseline.h
+++ spng/inc/WireCellSpng/Rebaseline.h
@@ -14,19 +14,20 @@ namespace WireCell::SPNG {
      * and last sample values.
      *
      * @param tensor The input tensor.
-     * @param min_roi_size The fragment must have this many samples above threshold.
-     * @param expand_size The number of samples to expand.
      * @param dim The dimension along which to operate (default: last dimension, -1).
+     * @param threshold The value above which a fragment is considered active.
+     * @param min_roi_size The fragment must have this many samples above threshold.
+     * @param expand_size Fixed number of samples to expand an initial ROI on either end.
      * @param remove_small If true, set ROIs failing min size to 0.0 value.
      * @param remove_negative If true, after rebaseline, set all below-threshold samples to 0.0 value.
-     * @param threshold The value above which a fragment is considered active.
      * @return The rebaselined tensor.
      */
     torch::Tensor rebaseline(const torch::Tensor& tensor,
+                             int64_t dim = -1, float threshold = 0.0f,
                              int64_t min_roi_size = 1,
-                             int64_t expand_size = 1,
-                             bool remove_small = true,
-                             bool remove_negative = true,
-                             int64_t dim = -1, float threshold = 0.0f);
+                             int64_t expand_size = 0,
+                             bool remove_small = false,
+                             bool remove_negative = false);
+
 }
 
