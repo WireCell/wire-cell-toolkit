0e4dfdaf3d1debc0133367cdf7db5f554d098b5b
Brett Viren <brett.viren@gmail.com>
2026-07-14 16:52:15 -0400

    Fix two tests that should never have worked.

--- util/test/doctest_response.cxx
+++ util/test/doctest_response.cxx
@@ -1,11 +1,17 @@
 #include "WireCellUtil/doctest.h"
 #include "WireCellUtil/Response.h"
 
+#include <cstdlib>
+
 using namespace WireCell;
 
 TEST_CASE("util field response")
 {
     std::string fr_file = "garfield-11impacts-dune.json.bz2";
+
+    const char* wcpath = std::getenv("WIRECELL_PATH");
+    // MESSAGE("WIRECELL_PATH=" << (wcpath ? wcpath : "(unset)"));
+    // std::cerr << wcpath << std::endl;
 
     auto fr = Response::Schema::load(fr_file.c_str());
     auto fravg = Response::wire_region_average(fr);
