79c4043d78c54926725ca52671306626058b09c2
Brett Viren <brett.viren@gmail.com>
2026-08-24 11:19:28 -0400

    Fix compiler warning->error due to unused vars.  Fixes #498.

--- util/test/doctest_response.cxx
+++ util/test/doctest_response.cxx
@@ -9,10 +9,10 @@ TEST_CASE("util field response")
 {
     std::string fr_file = "garfield-11impacts-dune.json.bz2";
 
-    const char* wcpath = std::getenv("WIRECELL_PATH");
+    /*const char* wcpath =*/ std::getenv("WIRECELL_PATH");
     // MESSAGE("WIRECELL_PATH=" << (wcpath ? wcpath : "(unset)"));
     // std::cerr << wcpath << std::endl;
 
-    auto fr = Response::Schema::load(fr_file.c_str());
-    auto fravg = Response::wire_region_average(fr);
+    /*auto fr =*/ Response::Schema::load(fr_file.c_str());
+    /*auto fravg =*/ Response::wire_region_average(fr);
 }
