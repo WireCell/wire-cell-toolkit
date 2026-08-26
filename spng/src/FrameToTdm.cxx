749fe8d0cc0851fee591c3bf1f91c424a95723c2
Brett Viren <brett.viren@gmail.com>
2026-08-26 14:44:56 -0400

    Apply Amit's fix.  Fixes #499.

--- spng/src/FrameToTdm.cxx
+++ spng/src/FrameToTdm.cxx
@@ -326,12 +326,12 @@ namespace WireCell::SPNG {
                     if (group_tirs.empty()) { // first time
                         tbeg = tbin;
                         tend = tbin+nbins;
-                        continue;
+                    }
+                    else {
+                      if (tbeg < tbin) { tbeg = tbin; }
+                      if (tend < tbin+nbins) { tend = tbin+nbins;}
                     }
                     group_tirs.emplace_back(TIR{trace, summary_index, row});
-
-                    if (tbeg < tbin) { tbeg = tbin; }
-                    if (tend < tbin+nbins) { tend = tbin+nbins;}
 
                 }
                 int nrows = group.chid2row.size(); // span all channels as traces may be sparse
